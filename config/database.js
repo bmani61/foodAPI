fd73cdcab3187fc0444665646f188c2e5d6dfd49
Manikanth <chinna.mrb@gmail.com>
2023-02-25 16:26:51 +0530

    set up initial API
    testing auth functionality

--- config/database.js
+++ config/database.js
@@ -1,31 +1,32 @@
-const mongoose = require( 'mongoose' ),
-    config = require( './config' ).getConfig();
+const mongoose = require("mongoose"),
+  config = require("./config").getConfig();
 
 // Mongo Connection Class
 class Connection {
-    constructor() {
-        const url = config.MONGO_URL;
+  constructor() {
+    const url = config.MONGO_URL;
 
-        mongoose.Promise = global.Promise;
-        mongoose.set( 'useNewUrlParser', true );
-        mongoose.set( 'useFindAndModify', false );
-        mongoose.set( 'useCreateIndex', true );
-        mongoose.set( 'useUnifiedTopology', true );
-        this.connect( url ).then( () => {
-            console.log( '✔ Database Connected' );
-        } ).catch( ( err ) => {
-            console.error( '✘ MONGODB ERROR: ', err.message );
-        } );
+    mongoose.Promise = global.Promise;
+    mongoose.set("useNewUrlParser", true);
+    mongoose.set("useFindAndModify", false);
+    mongoose.set("useCreateIndex", true);
+    mongoose.set("useUnifiedTopology", true);
+    this.connect(url)
+      .then(() => {
+        console.log("✔ Database Connected");
+      })
+      .catch((err) => {
+        console.error("✘ MONGODB ERROR: ", err.message);
+      });
+  }
 
+  async connect(url) {
+    try {
+      await mongoose.connect(url);
+    } catch (e) {
+      throw e;
     }
-
-    async connect( url ) {
-        try {
-            await mongoose.connect( url );
-        } catch ( e ) {
-            throw e;
-        }
-    }
+  }
 }
 
 module.exports = new Connection();
